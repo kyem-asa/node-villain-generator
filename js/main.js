4d4df06813df4bd4142ed72c86549f18158a3e78
Kyem Asa <turtlemoons@protonmail.com>
2022-12-20 12:15:12 -0600

    update fetch url

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ document.querySelector('#submit').addEventListener('click', makeReq)
 
 async function makeReq(){
 
-  // const userName = document.querySelector("#userName").value;
-  const res = await fetch(`/api`)
+  const res = await fetch(`https://villain-origin-generator.up.railway.app/api`)
   const data = await res.json()
 
   console.log(data);
