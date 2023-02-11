04f92135cb165b53301c78492fdc47cc896e667b
kyem-asa <turtlemoons@protonmail.com>
2023-02-10 22:41:57 -0600

    centered content remove quotes

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ async function makeReq(){
   const data = await res.json()
 
   console.log(data);
-  document.querySelector("#location").textContent = `You're villian era began at ${JSON.stringify(data.locations[Math.floor(Math.random() * data.locations.length)])}`
-  document.querySelector("#companions").textContent = `You were ${JSON.stringify(data.companions[Math.floor(Math.random() * data.companions.length)])}`
-  document.querySelector("#missions").textContent = `Your mission in life is now ${JSON.stringify(data.missions[Math.floor(Math.random() * data.missions.length)])}`
+  document.querySelector("#location").textContent = `Your villian era began at ${JSON.stringify(data.locations[Math.floor(Math.random() * data.locations.length)]).replace(/"|'/g, '')}`
+  document.querySelector("#companions").textContent = `You were ${JSON.stringify(data.companions[Math.floor(Math.random() * data.companions.length)]).replace(/"|'/g, '')}`
+  document.querySelector("#missions").textContent = `Your mission in life is now ${JSON.stringify(data.missions[Math.floor(Math.random() * data.missions.length)]).replace(/"|'/g, '')}`
 
 }
 
